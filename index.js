a4afd583c746100eff661cad3fbce91d64fef4a4
eunicelok <eunicelp511@gmail.com>
2020-01-09 17:44:40 +0800

    Redo change the program to take the kind of conversion you want to do.

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ console.log("hello");
 
 //Write a nodejs command line program that takes a hex color as an argument and returns the rbg CSS value.
 // convert hexToRgb
-// function hexToRgb(hex) {
-//   var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-//   return result ? {
-//     r: parseInt(result[1], 16),
-//     g: parseInt(result[2], 16),
-//     b: parseInt(result[3], 16)
-//   } : null;
-// }
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
 
 // console.log(hexToRgb(process.argv[3])); // "51";
 
@@ -25,19 +25,19 @@ function rgbToHex(r, g, b) {
   return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
-console.log(rgbToHex(parseInt(process.argv[3]), parseInt(process.argv[4]), parseInt(process.argv[5])));
+// console.log(rgbToHex(parseInt(process.argv[3]), parseInt(process.argv[4]), parseInt(process.argv[5])));
 
 //Change the program to take the kind of conversion you want to do.
-// if (process.argv[2] === "hex") {
-//     console.log(hexToRgb(process.argv[3]))
-// } else if (process.argv[2] === "rgb"){
-//     let r = parseInt(process.argv[3])
-//     let g = parseInt(process.argv[4])
-//     let b = parseInt(process.argv[5])
+if (process.argv[2] === "hex") {
+    console.log(hexToRgb(process.argv[3]))
+} else if (process.argv[2] === "rgb"){
+    let r = parseInt(process.argv[3])
+    let g = parseInt(process.argv[4])
+    let b = parseInt(process.argv[5])
 
-//     let rgb = rgbToHex(r,g,b)
-//     console.log(rgb);
-// }
+    let rgb = rgbToHex(r,g,b)
+    console.log(rgb);
+}
 
 // var my = require('./hextorgb')
 // console.log(my);
